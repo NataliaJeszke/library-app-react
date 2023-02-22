bb0cec5cd33eb3f4cf57e10038b7ce9a7d0ded7d
NataliaJeszke <nataliajeszke8@gmail.com>
2023-02-22 20:44:22 +0100

    form style

--- src/containers/BookMetadataInput/BookMetadataInput.jsx
+++ src/containers/BookMetadataInput/BookMetadataInput.jsx
@@ -1,7 +1,9 @@
-import formStyle from "./BookMetadataInputStyle.module.css";
 import { addBookAction } from "../../store/book/book-slice";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import bookFormStyle from "./BookMetadataInputStyle.module.css"
 
 export function BookMetadataInput(props) {
   const dispatch = useDispatch();
@@ -17,29 +19,31 @@ export function BookMetadataInput(props) {
     console.log("submit");
   }
   return (
-    <form onSubmit={submit} className={formStyle.metadataForm}>
-      <label>
+    <Form>
+    <form onSubmit={submit} className={bookFormStyle.formBox}>
+      <label className={bookFormStyle.title}>
         Title:
-        <input type="text" onChange={(event) => setTitle(event.target.value)} />
+        <input type="text" onChange={(event) => setTitle(event.target.value)} className="p-1 ms-4" />
       </label>
       <label>
         Author:
         <input
           type="text"
-          onChange={(event) => setAuthor(event.target.value)}
+          onChange={(event) => setAuthor(event.target.value)} className="p-1 ms-1" 
         />
       </label>
       <label>
         Date of publish:
-        <input type="text" onChange={(event) => setDate(event.target.value)} />
+        <input type="text" onChange={(event) => setDate(event.target.value)} className={bookFormStyle.date}/>
       </label>
       <label>
         Genre:
-        <input type="text" onChange={(event) => setGenre(event.target.value)} />
+        <input type="text" onChange={(event) => setGenre(event.target.value)} className="p-1 ms-3" />
       </label>
       <button type="submit" value="Submit">
         Add Book
       </button>
     </form>
+    </Form>
   );
 }
