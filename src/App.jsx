e6f2ccd12792852cb4aa5dcd48fd82ae7f8d1c24
NataliaJeszke <nataliajeszke8@gmail.com>
2023-02-22 22:01:30 +0100

    added Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { AddBook } from "./components/AddBook/AddBook";
 import { Header } from "./components/Header/Header";
 import { MainList } from "./components/MainList/MainList"
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function App() {
   return (
