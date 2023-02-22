089fdd5a5ec852b21856198767cc1d9f61e279ec
NataliaJeszke <nataliajeszke8@gmail.com>
2023-02-23 00:07:59 +0100

    header style

--- src/components/Header/LeftSideHero/LeftSideHero.jsx
+++ src/components/Header/LeftSideHero/LeftSideHero.jsx
@@ -1,5 +1,4 @@
 import { AddBook } from "../../AddBook/AddBook";
-import { ButtonContact } from "../ButtonContact/ButtonContact";
 import leftSideStyle from "./LeftSideStyle.module.css"
 export function LeftSideHero({}) {
     return (<div className={leftSideStyle.leftSide}>
