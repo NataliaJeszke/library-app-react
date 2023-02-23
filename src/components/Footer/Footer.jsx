2b6a5d8f619270141bca29ffb9d3ac05095dd627
NataliaJeszke <nataliajeszke8@gmail.com>
2023-02-24 00:05:26 +0100

    added link in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import footerStyle from "./FooterStyle.module.css";
+import { GoMarkGithub } from "react-icons/go";
 
 export function Footer(){
+    const gitURL = 'https://github.com/NataliaJeszke'
     return(
         <footer className={footerStyle.footer}>
-            <p>Check my other work on GitHub!</p>
+            <p>Check my other projects on <a href={gitURL}><GoMarkGithub style={{fontSize:'30px', color:'black'}}/></a></p>
         </footer>
     )
 }
--- src/components/MainList/MainList.jsx
+++ src/components/MainList/MainList.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { deleteBookByNanoID  } from "../../store/book/book-slice";
+import { deleteBookByNanoID } from "../../store/book/book-slice";
 import { BookComponent } from "../BookComponent/BookComponent";
 import { FilterBooks } from "../FilterBooks/FilterBooks";
 import { useState } from "react";
@@ -9,34 +9,46 @@ export function MainList() {
   const { bookList } = useSelector((state) => state.BOOK);
   console.log(bookList);
 
-  const [searchTerm, setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const filteredBooks = bookList.filter(item=>{
-    const contentTitle = item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredBooks = bookList.filter((item) => {
+    const contentTitle = item.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
     return contentTitle;
-  })
+  });
 
   const dispatch = useDispatch();
- 
- function handleRemove (id) {
 
+  function handleRemove(id) {
     dispatch(deleteBookByNanoID(id));
-  };
+  }
 
   return (
     <div className={listStyle.main}>
-    <FilterBooks onTextChange={setSearchTerm} placeholder="search by title"/>
-    <div>
-      {bookList.length>0 ? <h2 className={listStyle.h2}>You have {bookList.length} books on your Bookshelf </h2>:<h2 className={listStyle.h2}>Your Bookshelf is empty &#128532;</h2>}
-    </div>
+      <FilterBooks onTextChange={setSearchTerm} placeholder="search by title" />
+      <div>
+        {bookList.length > 0 ? (
+          <h2 className={listStyle.h2}>
+            You have {bookList.length} books on your Bookshelf{" "}
+          </h2>
+        ) : (
+          <h2 className={listStyle.h2}>Your Bookshelf is empty &#128532;</h2>
+        )}
+      </div>
       <div className={listStyle.mainDiv}>
         {filteredBooks.map((item, index) => (
           <div key={index} className={listStyle.outputDiv}>
             <BookComponent item={item} />
-            <button onClick={() => handleRemove(item.id)} className={listStyle.button}>Delete</button>
+            <button
+              onClick={() => handleRemove(item.id)}
+              className={listStyle.button}
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
-      </div>
+    </div>
   );
 }
